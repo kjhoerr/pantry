11264e508d887a0f8e4317963c0ee032b551023d
Kevin J Hoerr <kjhoerr@protonmail.com>
2022-01-25 23:57:08 +0000

    Add compose for MariaDB

---
+++ src/main/java/dev/submelon/pantry/ItemController.java
@@ -0,0 +1,33 @@
+package dev.submelon.pantry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/item")
+public class ItemController {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewItem (@RequestParam String name, @RequestParam String description, @RequestParam double quantity) {
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        return "Ok";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+    
+}
---
+++ src/main/java/dev/submelon/pantry/ItemRepository.java
@@ -0,0 +1,7 @@
+package dev.submelon.pantry;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, Integer> {
+    
+}
