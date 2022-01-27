8d8d8cdd639591c0c4c49b355db1152e310c22d6
Kevin J Hoerr <khoerr@ksmpartners.com>
2022-01-27 13:57:10 -0500

    Revert to Java 11 and enable Lombok ext

--- src/main/java/dev/submelon/pantry/PantryApplication.java
+++ src/main/java/dev/submelon/pantry/PantryApplication.java
@@ -2,17 +2,11 @@ package dev.submelon.pantry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class PantryApplication {
-
-  @RequestMapping("/")
-  public String home() {
-    return "Check this out: http://localhost:8080/items";
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(PantryApplication.class, args);
