cdf65b32202746eaffd9e58bf951d1995ab03be3
Kevin J Hoerr <khoerr@ksmpartners.com>
2022-01-31 16:06:42 -0500

    Add DELETE endpoint for PantryItem

--- src/main/java/dev/submelon/pantry/PantryItemController.java
+++ src/main/java/dev/submelon/pantry/PantryItemController.java
@@ -1,7 +1,10 @@
 package dev.submelon.pantry;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PantryItemController {
     @Autowired
     private PantryItemRepository itemRepository;
+
+    @GetMapping(path="")
+    @ResponseBody
+    Iterable<PantryItem> getAllItems() {
+        return itemRepository.findAll();
+    }
 
     @PostMapping(path="")
     @ResponseBody
@@ -38,10 +47,15 @@ public class PantryItemController {
             });
     }
 
-    @GetMapping(path="")
+    @DeleteMapping(path="/{id}")
     @ResponseBody
-    Iterable<PantryItem> getAllItems() {
-        return itemRepository.findAll();
+    PantryItem deletePantryItem(@PathVariable Long id) {
+        return itemRepository.findById(id)
+            .map(item -> {
+                itemRepository.delete(item);
+                return item;
+            })
+            .orElseThrow(NoSuchElementException::new);
     }
     
 }
