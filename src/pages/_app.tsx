1b42afd8c5eb9e16c6f7e93c0518cee1285ec383
Kevin J Hoerr <kjhoerr@protonmail.com>
2022-08-15 00:50:04 +0000

    Add test data and link to q/dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      {process.env.APP_ENV === "development" ? (<div style={{textAlign: "center", marginTop: "14px"}}><a href="/q/dev/">Visit Quarkus dev page</a></div>) : undefined}
     </QueryClientProvider>
   );
 }
