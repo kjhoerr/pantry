df679adf90548b20dfd1532cde406f14358df85f
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-05-06 10:27:19 -0400

    codegen

--- src/main/webui/src/gql/conf/graphql.ts
+++ src/main/webui/src/gql/conf/graphql.ts
@@ -28,6 +28,8 @@ export type Mutation = {
   deleteItem?: Maybe<Scalars["Boolean"]>;
   /** Store an item in the pantry */
   storeItem?: Maybe<PantryItem>;
+  /** Create any new labels from list of labels */
+  syncLabels?: Maybe<Array<Maybe<PantryItemLabel>>>;
 };
 
 /** Mutation root */
@@ -40,10 +42,16 @@ export type MutationStoreItemArgs = {
   item?: InputMaybe<PantryItemInput>;
 };
 
+/** Mutation root */
+export type MutationSyncLabelsArgs = {
+  labels?: InputMaybe<Array<InputMaybe<PantryItemLabelInput>>>;
+};
+
 export type PantryItem = {
   __typename?: "PantryItem";
   description?: Maybe<Scalars["String"]>;
   id?: Maybe<Scalars["String"]>;
+  labels?: Maybe<Array<Maybe<Scalars["String"]>>>;
   name?: Maybe<Scalars["String"]>;
   quantity: Scalars["Float"];
   quantityUnitType?: Maybe<Scalars["String"]>;
@@ -52,9 +60,23 @@ export type PantryItem = {
 export type PantryItemInput = {
   description?: InputMaybe<Scalars["String"]>;
   id?: InputMaybe<Scalars["String"]>;
+  labels?: InputMaybe<Array<InputMaybe<Scalars["String"]>>>;
   name?: InputMaybe<Scalars["String"]>;
   quantity: Scalars["Float"];
   quantityUnitType?: InputMaybe<Scalars["String"]>;
+};
+
+export type PantryItemLabel = {
+  __typename?: "PantryItemLabel";
+  color?: Maybe<Scalars["String"]>;
+  id?: Maybe<Scalars["String"]>;
+  title?: Maybe<Scalars["String"]>;
+};
+
+export type PantryItemLabelInput = {
+  color?: InputMaybe<Scalars["String"]>;
+  id?: InputMaybe<Scalars["String"]>;
+  title?: InputMaybe<Scalars["String"]>;
 };
 
 /** Query root */
@@ -62,6 +84,8 @@ export type Query = {
   __typename?: "Query";
   /** Get all items stored in the pantry */
   allItems?: Maybe<Array<Maybe<PantryItem>>>;
+  /** Get all labels that can be assigned to items */
+  allLabels?: Maybe<Array<Maybe<PantryItemLabel>>>;
   /** Get an item stored in the pantry */
   item?: Maybe<PantryItem>;
 };
@@ -103,6 +127,34 @@ export type StoreItemMutation = {
     quantity: number;
     quantityUnitType?: string | null;
   } | null;
+};
+
+export type AllLabelsQueryVariables = Exact<{ [key: string]: never }>;
+
+export type AllLabelsQuery = {
+  __typename?: "Query";
+  allLabels?: Array<{
+    __typename?: "PantryItemLabel";
+    id?: string | null;
+    title?: string | null;
+    color?: string | null;
+  } | null> | null;
+};
+
+export type SyncLabelsMutationVariables = Exact<{
+  labels?: InputMaybe<
+    Array<InputMaybe<PantryItemLabelInput>> | InputMaybe<PantryItemLabelInput>
+  >;
+}>;
+
+export type SyncLabelsMutation = {
+  __typename?: "Mutation";
+  syncLabels?: Array<{
+    __typename?: "PantryItemLabel";
+    id?: string | null;
+    title?: string | null;
+    color?: string | null;
+  } | null> | null;
 };
 
 export const AllItemsDocument = {
@@ -259,3 +311,83 @@ export const StoreItemDocument = {
     },
   ],
 } as unknown as DocumentNode<StoreItemMutation, StoreItemMutationVariables>;
+export const AllLabelsDocument = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "query",
+      name: { kind: "Name", value: "allLabels" },
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "allLabels" },
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                { kind: "Field", name: { kind: "Name", value: "id" } },
+                { kind: "Field", name: { kind: "Name", value: "title" } },
+                { kind: "Field", name: { kind: "Name", value: "color" } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<AllLabelsQuery, AllLabelsQueryVariables>;
+export const SyncLabelsDocument = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "mutation",
+      name: { kind: "Name", value: "syncLabels" },
+      variableDefinitions: [
+        {
+          kind: "VariableDefinition",
+          variable: {
+            kind: "Variable",
+            name: { kind: "Name", value: "labels" },
+          },
+          type: {
+            kind: "ListType",
+            type: {
+              kind: "NamedType",
+              name: { kind: "Name", value: "PantryItemLabelInput" },
+            },
+          },
+        },
+      ],
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "syncLabels" },
+            arguments: [
+              {
+                kind: "Argument",
+                name: { kind: "Name", value: "labels" },
+                value: {
+                  kind: "Variable",
+                  name: { kind: "Name", value: "labels" },
+                },
+              },
+            ],
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                { kind: "Field", name: { kind: "Name", value: "id" } },
+                { kind: "Field", name: { kind: "Name", value: "title" } },
+                { kind: "Field", name: { kind: "Name", value: "color" } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<SyncLabelsMutation, SyncLabelsMutationVariables>;
