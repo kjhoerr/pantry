1ed04646cdcf931cf2f6002ec5d2305d45743e22
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-12-04 04:32:26 +0000

    Add test for add item component for cancel

--- src/test/java/webui/ApplicationTest.java
+++ src/test/java/webui/ApplicationTest.java
@@ -1,14 +1,18 @@
 package webui;
 
-import java.net.URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Assertions;
+import java.net.URL;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.*;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Response;
+import com.microsoft.playwright.options.AriaRole;
 
 import io.quarkiverse.playwright.InjectPlaywright;
 import io.quarkiverse.playwright.WithPlaywright;
@@ -22,6 +26,14 @@ import io.quarkus.test.junit.TestProfile;
 @TestProfile(QuinoaTestProfiles.Enable.class)
 public class ApplicationTest {
 
+    public static final String addItemLabel = "Add Item";
+    public static final String itemNameLabel = "Item Name";
+    public static final String itemDescriptionLabel = "Item Description";
+    public static final String itemQuantityLabel = "Item Quantity";
+    public static final String itemQuantityTypeLabel = "Quantity Type";
+    public static final String submitItemLabel = "Submit Item";
+    public static final String cancelLabel = "Cancel";
+
     @InjectPlaywright
     BrowserContext context;
 
@@ -30,22 +42,105 @@ public class ApplicationTest {
 
     @Test
     public void testLanding() {
+        final Page page = loadAndVerifyLandingPage();
+
+        checkItemTableIsEmpty(page);
+
+        // Check button is visible and enabled
+        Locator addButton = page.getByRole(AriaRole.BUTTON,
+               new Page.GetByRoleOptions().setName(addItemLabel));
+        assertThat(addButton).isVisible();
+        assertThat(addButton).isEnabled();
+    }
+
+    @Test
+    public void testAddItemCancel() {
+        final Page page = loadAndVerifyLandingPage();
+
+        checkItemTableIsEmpty(page);
+
+        // Check button is visible and enabled
+        Locator addButton = page.getByRole(AriaRole.BUTTON,
+               new Page.GetByRoleOptions().setName(addItemLabel));
+        assertThat(addButton).isVisible();
+        assertThat(addButton).isEnabled();
+
+        // make available add item component
+        addButton.click();
+
+        // verify initial state of inputs and buttons
+        assertThat(addButton).isHidden();
+        Locator nameInput = page.getByLabel(itemNameLabel);
+        Locator descriptionInput = page.getByLabel(itemDescriptionLabel);
+        Locator quantityInput = page.getByLabel(itemQuantityLabel);
+        Locator quantityTypeInput = page.getByLabel(itemQuantityTypeLabel);
+        assertThat(nameInput).isVisible();
+        assertThat(nameInput).isEditable();
+        assertThat(descriptionInput).isVisible();
+        assertThat(descriptionInput).isEditable();
+        assertThat(quantityInput).isVisible();
+        assertThat(quantityInput).isEditable();
+        assertThat(quantityTypeInput).isVisible();
+        assertThat(quantityTypeInput).isEditable();
+
+        Locator submitButton = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName(submitItemLabel));
+        Locator cancelButton = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName(cancelLabel));
+        assertThat(submitButton).isVisible();
+        assertThat(submitButton).isDisabled();
+        assertThat(cancelButton).isVisible();
+        assertThat(cancelButton).isEnabled();
+
+        // test initial input and closing component
+        nameInput.type("Flour");
+        descriptionInput.type("White unbleached");
+
+        assertThat(submitButton).isEnabled();
+
+        cancelButton.click();
+        
+        // verify add item component is closed
+        assertThat(nameInput).isHidden();
+        assertThat(descriptionInput).isHidden();
+        assertThat(quantityInput).isHidden();
+        assertThat(quantityTypeInput).isHidden();
+        assertThat(submitButton).isHidden();
+        assertThat(cancelButton).isHidden();
+
+        checkItemTableIsEmpty(page);
+    }
+
+
+    /**
+     * Navigate to page with table of Pantry Items
+     */
+    private Page loadAndVerifyLandingPage() {
         final Page page = context.newPage();
         Response response = page.navigate(index.toString());
-        Assertions.assertEquals("OK", response.statusText());
+        assertEquals("OK", response.statusText());
 
         page.waitForLoadState();
 
-        String title = page.title();
-        Assertions.assertEquals("Pantry", title);
+        assertThat(page).hasTitle("Pantry");
 
-        // Check table is empty with appropriate message
-        String tableMessage = page.innerText("#tbl-msg-empty");
-        Assertions.assertEquals("Nothing's in the pantry at the moment!", tableMessage);
+        return page;
+    }
 
-        // Check button is visible and enabled
-        Locator addButton = page.locator("button#pantry-add-item");
-        Assertions.assertTrue(addButton.isVisible());
-        Assertions.assertTrue(addButton.isEnabled());
+    /**
+     * Check table is empty with appropriate message
+     */
+    private void checkItemTableIsEmpty(final Page page) {
+        assertThat(page.locator("#tbl-msg-empty"))
+                .containsText("Nothing's in the pantry at the moment!");
+    }
+
+    /**
+     * Assert that a notification is displayed with the specified header and text
+     */
+    private void validateNotification(final Page page, final String header, final String text) {
+        Locator specifiedNotification = page.locator("div#toast-holder").getByRole(AriaRole.ALERT).filter(new Locator.FilterOptions().setHasText(text));
+        assertThat(specifiedNotification).isVisible();
+        assertThat(specifiedNotification).containsText(header);
     }
 }
