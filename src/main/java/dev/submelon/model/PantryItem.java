636f78543bd5aa73e0518c1c15bbd280362e365c
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-12-05 03:58:32 +0000

    Add NoArgConstructor, AllArgsConstructor for PantryItem

--- src/main/java/dev/submelon/model/PantryItem.java
+++ src/main/java/dev/submelon/model/PantryItem.java
@@ -5,12 +5,16 @@ import org.bson.types.ObjectId;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntityBase;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 @MongoEntity(collection = "item")
 public class PantryItem extends ReactivePanacheMongoEntityBase {
