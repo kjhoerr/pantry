94320d4b1e4427f902a86cca750b15f184358910
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-11-01 14:38:37 -0400

    shell.nix: Remove unused chromium environment variable

--- shell.nix
+++ shell.nix
@@ -6,9 +6,6 @@
     ];
 
     shellHook = ''
-      playwright_chromium_revision="$(${pkgs.jq}/bin/jq --raw-output '.browsers[] | select(.name == "chromium").revision' ${pkgs.playwright-driver}/browsers.json)"
-
-      export PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/chromium-$playwright_chromium_revision/chrome-linux/chrome";
       export PLAYWRIGHT_NODEJS_PATH=${pkgs.nodejs_20}/bin/node
       export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
       export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
