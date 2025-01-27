9ffcb06e7f8f99006f8cfbf90072c52833cf059b
Kevin J Hoerr <khoerr@ksmpartners.com>
2025-01-27 15:52:31 -0500

    Fix rsbuild eslint config

--- src/main/webui/rsbuild.config.ts
+++ src/main/webui/rsbuild.config.ts
@@ -4,7 +4,16 @@ import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default defineConfig({
-  plugins: [pluginEslint(), pluginReact(), pluginSvgr()],
+  plugins: [
+    pluginEslint({
+      eslintPluginOptions: {
+        cwd: __dirname,
+        configType: "flat",
+      },
+    }),
+    pluginReact(),
+    pluginSvgr(),
+  ],
   html: {
     template: "./public/index.html",
   },
