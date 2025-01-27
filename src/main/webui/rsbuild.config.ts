90d1a2e0d38f3ef8fa330aa4d26f884b9862620b
Kevin J Hoerr <khoerr@ksmpartners.com>
2025-01-27 17:22:11 -0500

    Add rsbuild-plugin-tailwindcss

--- src/main/webui/rsbuild.config.ts
+++ src/main/webui/rsbuild.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginEslint } from "@rsbuild/plugin-eslint";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginSvgr } from "@rsbuild/plugin-svgr";
+import { pluginTailwindCSS } from "rsbuild-plugin-tailwindcss";
 
 export default defineConfig({
   plugins: [
@@ -13,6 +14,7 @@ export default defineConfig({
     }),
     pluginReact(),
     pluginSvgr(),
+    pluginTailwindCSS(),
   ],
   html: {
     template: "./public/index.html",
